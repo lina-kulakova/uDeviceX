954d782a8e7ea7e5c87da38ec330c48e1ad14289
Sergey Litvinov <slitvinov@gmail.com>
2015-08-29 11:47:54 +0200

    Add altransformio and piped versions of utilities

---
+++ configs/aldriver-pipe.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#set -eu
+
+# Usage:
+#   cat <parameter lines> | ./aldriver2.sh <source directory>
+
+set -eu
+
+source_directory=$1
+tmp_source_directory=`mktemp -d /tmp/alldriver.XXXX`
+alpachio_config=`mktemp /tmp/alpachio.XXXX`
+cart_list=`mktemp /tmp/cartlist.XXXX`
+
+function msg() {
+    printf "(aldriver.sh) %s\n" "$@"
+}
+
+function err() {
+    msg "$@"
+    exit 2
+}
+
+test -d "$source_directory" || \
+    err "\"$source_directory\" is not a directory"
+
+
+function run_case() {
+    (cd "$tmp_source_directory"
+     bash configs/setup_daint.sh)
+}
+
+function create_case() {
+    msg "create_case: $source_directory $tmp_source_directory"    
+    test -d "$tmp_source_directory" && rm -rf "$tmp_source_directory"
+    cp -r "$source_directory" "$tmp_source_directory"
+
+    msg "config file: $alpachio_config"
+    ./allineario.awk "$1" > "$alpachio_config"
+
+    ./alpachio.sh "$alpachio_config" \
+		  "$tmp_source_directory"/configs/setup_daint.sh \
+		  "$tmp_source_directory"/mpi-dpd/common.h
+    run_case
+}
+
+while read -r line; do
+    create_case "${line}"
+done
