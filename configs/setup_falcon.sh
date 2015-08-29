1564858acdff53b8256c3cb96d96082d9f0750ce
Sergey Litvinov <slitvinov@gmail.com>
2015-08-29 17:46:24 +0200

    Fixed makefiles to compile on panda

--- configs/setup_falcon.sh
+++ configs/setup_falcon.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 cp configs/Makefile.falcon mpi-dpd/.cache.Makefile
+cp configs/Makefile.falcon cuda-ctc/.cache.Makefile
 
 opath=$PATH
 
 PATH=/usr/local/cuda/bin/:$PATH
 
 cd mpi-dpd
-make clean && make -j slevel="-2"
+make -j clean && make slevel="-2"
+cd -
+
+./mpi-dpd/test 1 1 1 -rbc -stretching_force 10
