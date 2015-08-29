e012e2652bf53f18b4b68b7b998d8835d0fa72b6
Sergey Litvinov <slitvinov@gmail.com>
2015-08-29 20:50:05 +0200

    Make setup_falcon modular

--- configs/setup_falcon.sh
+++ configs/setup_falcon.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-cp configs/Makefile.falcon mpi-dpd/.cache.Makefile
-cp configs/Makefile.falcon cuda-ctc/.cache.Makefile
-
 opath=$PATH
-
 PATH=/usr/local/cuda/bin/:$PATH
 
-cd mpi-dpd
-make -j clean && make slevel="-2"
-cd -
+function err() {
+    printf "(setup_falcon.sh) $@\n"
+    exit
+}
 
-./mpi-dpd/test 1 1 1 -rbc -stretching_force 10
+function compile() {
+    cp configs/Makefile.falcon mpi-dpd/.cache.Makefile
+    cp configs/Makefile.falcon cuda-ctc/.cache.Makefile
+    cd mpi-dpd
+    make -j clean && make slevel="-2"
+    cd -
+}
+
+function run() {
+    ./mpi-dpd/test 1 1 1 -rbc -stretching_force 10
+}
+
+compile
+run
