b6783e9f692bfcc0f9b94a34d115c09a11c869d7
Sergey Litvinov <slitvinov@gmail.com>
2015-08-30 18:16:41 +0200

    Update backup.sh

--- configs/backup.sh
+++ configs/backup.sh
@@ -6,5 +6,7 @@ suffix=_bak_
 for f ; do
     org="$f"
     bak="$f".$suffix
-    test -r "$org" && cp "$org" "$bak"
+    # do not overwrite backup
+    test -r "$bak" || cp "$org" "$bak"
+    test -r "$bak" && echo "(backup.sh) I see backup file: $bak"
 done
