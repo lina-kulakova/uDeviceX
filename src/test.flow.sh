c2c746f2ac8f9e145f7b0143da670f5fce647673
Sergey Litvinov <slitvinov@gmail.com>
2017-03-04 17:48:38 +0100

    Remove the rest of ply related files

--- src/test.flow.sh
+++ src/test.flow.sh
@@ -11,44 +11,44 @@
 # TEST: double.poiseuille
 # set -x
 # export PATH=../tools:$PATH
-# rm -rf ply h5 diag.txt
+# rm -rf h5 diag.txt
 # argp .conf.double.poiseuille.h     \
 #   -tend=2.0 -steps_per_dump=4000    \
 #   -pushtheflow -doublepoiseuille   \
 #   -hdf5field_dumps -hdf5part_dumps \
 #   -steps_per_hdf5dump=4000 -pushtheflow > .conf.h
-# make clean && make -j && make -C ../tools
+# make clean && make -j
 # ./udx
 # avg_h52.m h5/flowfields-0001.h5 | fround.awk -v tol=1 > h5.out.txt
 
 ####
 # TEST: diag.t4
 # export PATH=../tools:$PATH
-# rm -rf ply h5 diag.txt
+# rm -rf h5 diag.txt
 # cp sdf/wall1/wall.dat sdf.dat
 # argp .conf.test.h  \
 #   -tend=1.0 -steps_per_dump=300 -walls -wall_creation_stepid=100 \
 #   -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=300 > .conf.h
-# make clean && make -j && make -C ../tools
+# make clean && make -j
 # ./udx
 # avg_h5.m h5/flowfields-0003.h5 | fround.awk -v tol=1 > h5.out.txt
 
 ####
 # TEST: diag.t5
 # export PATH=../tools:$PATH
-# rm -rf ply h5 diag.txt
+# rm -rf h5 diag.txt
 # cp sdf/wall1/wall.dat sdf.dat
 # argp .conf.poiseuille.h \
 #   -tend=2.0 -steps_per_dump=300 -walls -wall_creation_stepid=100 \
 #   -hdf5field_dumps -hdf5part_dumps -steps_per_hdf5dump=300 -pushtheflow > .conf.h
-# make clean && make -j && make -C ../tools
+# make clean && make -j
 # ./udx
 # avg_h5.m h5/flowfields-0013.h5 | fround.awk -v tol=2 > h5.out.txt
 
 ####
 # TEST: diag.t6
 # export PATH=../tools:$PATH
-# rm -rf ply h5 diag.txt
+# rm -rf h5 diag.txt
 # cp sdf/cyl1/cyl.dat sdf.dat
 # argp .conf.poiseuille.h \
 #   -tend=4.0 -steps_per_dump=300 -walls -wall_creation_stepid=100 \
