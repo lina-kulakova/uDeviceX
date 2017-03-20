52dae4c6ccf211af5443d539a7b0fc827221827a
Sergey Litvinov <slitvinov@gmail.com>
2017-03-20 12:50:41 +0100

    Calling device function

--- poc/dev.hd/main.cu
+++ poc/dev.hd/main.cu
@@ -1,10 +1,27 @@
 #include "stdio.h"
+#include "assert.h"
 #include <hd2.h>
 
+__global__ void
+main_dev(float *a, float *b, float *ans) {
+  fun(a, b, ans);
+}
+
 int main() {
-  float a[3] = { 1,  2,  3};
-  float b[3] = {10, 20, 30};
+  float a[] = { 1,  2,  3};
+  float b[] = {10, 20, 30};
   float ans;
-  fun(a, b, /**/ &ans);
+
+  float *a_dev, *b_dev, *ans_dev;
+  int sz1 = sizeof(ans), sz3 = sizeof(a);
+
+  cudaMalloc(&a_dev, sz3); cudaMalloc(&b_dev, sz3);
+  cudaMalloc(&ans_dev, sz1);
+
+  cudaMemcpy(a_dev, a, sz3, cudaMemcpyHostToDevice);
+  cudaMemcpy(b_dev, b, sz3, cudaMemcpyHostToDevice);
+  main_dev<<<1,1>>>(a_dev, b_dev, ans_dev);
+  cudaMemcpy(&ans, ans_dev, sz1, cudaMemcpyDeviceToHost);
+
   printf("ans: %g\n", ans);
 }
