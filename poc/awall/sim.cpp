5080ff94c564899d00ee6c5a6ff6df2c88375a0b
Sergey Litvinov <slitvinov@gmail.com>
2017-03-27 18:56:36 +0200

    Minor

--- poc/awall/sim.cpp
+++ poc/awall/sim.cpp
@@ -3,12 +3,11 @@
 
 #define MAX_NP 100000 /* maximum number of particles */
 
-#define dir "xyz"
+#define dir "xyz"     /* output directory and file name */
 #define fmt "%03ld.3d"
 
-double xx[MAX_NP], yy[MAX_NP], zz[MAX_NP];
-double xxn[MAX_NP], yyn[MAX_NP], zzn[MAX_NP]; /* "next" particles
-						 position */
+double xx[MAX_NP],  yy[MAX_NP], zz[MAX_NP],
+      xxn[MAX_NP], yyn[MAX_NP], zzn[MAX_NP]; /* "next" particle position */
 double vvx[MAX_NP], vvy[MAX_NP], vvz[MAX_NP]; /* velocity */
 long type[MAX_NP];
 
@@ -21,7 +20,7 @@ double xc, yc, zc; /* center */
 double vx0, vy0, vz0;
 long   type0;
 
-long  ts, ns, nf;
+long  ts, ns;
 double dt;
 
 void init_vars() {
@@ -31,12 +30,11 @@ void init_vars() {
   Lx = xh - xl; Ly = yh - yl; Lz = zh - zl;
   xc = 0.5*(xh + xl); yc = 0.5*(yh + yl); zc = 0.5*(zh + zl);
 
-  vx0 = -2; vy0 = 0; vz0 = 0; /* velocity of the particles */
+  vx0 = -2; vy0 = 0; vz0 = 0; /* initial velocity */
   type0 = 0;                  /* initial type  */
   ts = 0;    /* current time frame (0, 1, 2, ...) */
   ns =  100;   /* number of time steps to make */
   dt = 0.1;   /* time steps */
-  nf = 1;    /* number of faces */
 
   system("mkdir " dir);
 }
@@ -91,14 +89,14 @@ void upd_vel() {
   /*  do nothing: keep constant velocity */
 }
 
-double wrp(double r, double c, double L) {
+double wrp(double r, double c, double L) { /* wrap back to the domain */
   auto dr = r - c;
   if      (2*dr >  L) return r - L;
   else if (2*dr < -L) return r + L;
   else                return r;
 }
 
-void pbc() {
+void pbc() { /* periodic boundary conditions */
     for (long ip = 0; ip < np; ip++) {
       xx[ip] = wrp(xx[ip], xc, Lx);
       yy[ip] = wrp(yy[ip], yc, Ly);
@@ -119,11 +117,11 @@ void upd_pos() { /* new to old */
     }
 }
 
-void upd() {
+void step() {
   new_pos(); /* get new position in `[xyz]n' */
-  bounce(); /* bouncing of the face */
+  bounce(); /* bouncing back */
   upd_pos(); /* [xyz] = [xyz]n */
-  pbc();     /* apply periodic BC */
+  pbc();     /* periodic BC */
   upd_vel(); /* get new velocity */
   ts += 1; /* update timestep */
 }
